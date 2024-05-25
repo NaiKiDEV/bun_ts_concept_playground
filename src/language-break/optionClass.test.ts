6c2f6446ff0023d7cec96e775a74671709738288
NaiKiDEV <pc999111@gmail.com>
2024-05-25 23:26:27 +0300

    feat: added option class implementation for option approach in typescript with tests

---
+++ src/language-break/optionClass.test.ts
@@ -0,0 +1,109 @@
+import { mock, expect, describe, it } from "bun:test";
+import {
+  createOptionC,
+  invokeAsyncActionC,
+  invokeSyncActionC,
+} from "./optionClass";
+
+describe("class based option", () => {
+  describe("isNone", () => {
+    it("should return true when object contains error prop", () => {
+      const error = new Error("error");
+      const option = createOptionC(error);
+
+      expect(option.isNone()).toBe(true);
+    });
+
+    it("should return false when object does not contain error prop", () => {
+      const option = createOptionC("foo");
+
+      expect(option.isNone()).toBe(false);
+    });
+  });
+
+  describe("isSome", () => {
+    it("should return true when object contains value prop", () => {
+      const option = createOptionC("foo");
+
+      expect(option.isSome()).toBe(true);
+    });
+
+    it("should return false when object does not contain value prop", () => {
+      const error = new Error("error");
+      const option = createOptionC(error);
+
+      expect(option.isSome()).toBe(false);
+    });
+  });
+
+  describe("unwrapValue", () => {
+    it("should return value when object contains value prop", () => {
+      const option = createOptionC("foo");
+
+      expect(option.unwrapValue()).toBe("foo");
+    });
+
+    it("should throw error when object does not contain value prop", () => {
+      const error = new Error("error");
+      const option = createOptionC(error);
+
+      expect(() => option.unwrapValue()).toThrowError("Option is None");
+    });
+  });
+
+  describe("unwrapError", () => {
+    it("should return error when object contains error prop", () => {
+      const error = new Error("error");
+      const option = createOptionC(error);
+
+      expect(option.unwrapError()).toBe(error);
+    });
+
+    it("should throw error when object does not contain error prop", () => {
+      const option = createOptionC("foo");
+
+      expect(() => option.unwrapError()).toThrowError("Option is Some");
+    });
+  });
+
+  describe("invokeSyncActionC", () => {
+    it("should return option with value when callback does not throw", () => {
+      const mockCallback = mock();
+      const option = invokeSyncActionC(mockCallback);
+
+      expect(mockCallback).toBeCalledTimes(1);
+      expect(option.isSome()).toBe(true);
+    });
+
+    it("should return option with error when callback throws", () => {
+      const mockCallback = mock(() => {
+        throw new Error("Mock Error");
+      });
+      const option = invokeSyncActionC(mockCallback);
+
+      expect(mockCallback).toBeCalledTimes(1);
+      expect(option.isNone()).toBe(true);
+    });
+  });
+
+  describe("invokeAsyncActionC", () => {
+    it("should return option with value when callback does not throw", async () => {
+      const mockCallback = mock();
+      const option = await invokeAsyncActionC(mockCallback);
+
+      expect(mockCallback).toBeCalledTimes(1);
+      expect(option.isSome()).toBe(true);
+    });
+
+    it("should return option with error when callback throws", async () => {
+      const mockCallback = mock(() => {
+        throw new Error("Mock Error");
+      });
+
+      const option = await invokeAsyncActionC(mockCallback);
+
+      expect(mockCallback).toBeCalledTimes(1);
+      expect(option.isNone()).toBe(true);
+    });
+  });
+});
--- src/language-break/optionClass.ts
+++ src/language-break/optionClass.ts
@@ -1,2 +1,121 @@
-// implement option class with builder pattern
-export {};
+enum OptionType {
+  Some,
+  None,
+}
+
+class OptionC<T, E extends Error> {
+  private value: T | null = null;
+  private error: E | null = null;
+  private optionType: OptionType | undefined;
+
+  constructor(value: T | E | Error) {
+    if (
+      value instanceof Error ||
+      (typeof value === "object" &&
+        value &&
+        "message" in value &&
+        "name" in value)
+    ) {
+      this.error = value as E;
+      this.optionType = OptionType.None;
+    } else {
+      this.value = value as T;
+      this.optionType = OptionType.Some;
+    }
+  }
+
+  unwrapValue(): T {
+    if (this.value === null) {
+      throw new Error("Option is None");
+    }
+
+    return this.value;
+  }
+
+  unwrapError(): E | Error {
+    if (this.error === null) {
+      throw new Error("Option is Some");
+    }
+
+    return this.error;
+  }
+
+  isSome(): boolean {
+    return this.optionType === OptionType.Some;
+  }
+
+  isNone(): boolean {
+    return this.optionType === OptionType.None;
+  }
+}
+
+const createOptionC = <T, E extends Error>(
+  value: T | E | Error,
+): OptionC<T, E> => {
+  return new OptionC<T, E>(value);
+};
+
+const invokeSyncActionC = <T, E extends Error>(
+  callback: () => T,
+): OptionC<T, E> => {
+  try {
+    const result = callback();
+
+    return createOptionC<T, E>(result);
+  } catch (error) {
+    if (
+      typeof error === "object" &&
+      error &&
+      "message" in error &&
+      "name" in error
+    ) {
+      return createOptionC<T, E>(error as E);
+    }
+
+    return createOptionC<T, E>(
+      new Error(`Illegal error thrown: ${typeof error} = '${error}'`),
+    );
+  }
+};
+
+const createLazySyncActionC =
+  <T, E extends Error>(callback: () => T) =>
+  () =>
+    invokeSyncActionC<T, E>(callback);
+
+const invokeAsyncActionC = async <T, E extends Error>(
+  callback: () => Promise<T>,
+): Promise<OptionC<T, E>> => {
+  try {
+    const result = await callback();
+
+    return createOptionC<T, E>(result);
+  } catch (error) {
+    if (
+      typeof error === "object" &&
+      error &&
+      "message" in error &&
+      "name" in error
+    ) {
+      return createOptionC<T, E>(error as E);
+    }
+
+    return createOptionC<T, E>(
+      new Error(`Illegal error thrown: ${typeof error} = '${error}'`),
+    );
+  }
+};
+
+const createLazyAsyncActionC =
+  <T, E extends Error>(callback: () => Promise<T>) =>
+  () =>
+    invokeAsyncActionC<T, E>(callback);
+
+export {
+  OptionC,
+  createOptionC,
+  invokeSyncActionC,
+  createLazySyncActionC,
+  invokeAsyncActionC,
+  createLazyAsyncActionC,
+};
